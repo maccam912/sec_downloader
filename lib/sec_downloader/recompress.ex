91e0840f910994d9862be6069c7720bbdfab9c22
maccam912 <maccam912@gmail.com>
2021-03-31 18:52:25 -0500

    use flow for recompressing

--- lib/sec_downloader/recompress.ex
+++ lib/sec_downloader/recompress.ex
@@ -1,15 +1,19 @@
 defmodule SecDownloader.Recompress do
   def run() do
-    fnames = File.ls!("filings")
-    |> Enum.filter(fn fname -> not String.contains?(fname, ".gz") end)
+    fnames =
+      File.ls!("filings")
+      |> Enum.filter(fn fname -> not String.contains?(fname, ".gz") end)
+
+    SecDownloader.Counter.start_link([])
 
     fnames
-    |> Stream.map(fn fname ->
+    |> Flow.from_enumerable()
+    |> Flow.map(fn fname ->
       f = File.read!("filings/#{fname}")
       File.write!("filings/#{fname}.gz", f, [:compressed])
       File.rm!("filings/#{fname}")
+      SecDownloader.Counter.inc()
     end)
-    |> Tqdm.tqdm(total: length(fnames))
-    |> Stream.run()
+    |> Flow.run()
   end
 end
